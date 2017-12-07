e17ce0831e1c5a04bb1f697076fe552103741bea
Ben Weinshel <ben@weinshel.net>
2017-12-07 15:37:18 -0600

    github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter,
   Route,
   Link
 } from 'react-router-dom';
@@ -11,13 +11,12 @@ import EmojiList from  './Emojis';
 class App extends Component {
   render() {
     return(
-      <Router>
+      <HashRouter>
         <div>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/chars">Characters</Link></li>
             <li><Link to="/emoji">Emoji</Link></li>
-            <li><Link to="/topics">Topics</Link></li>
           </ul>
 
           <hr/>
@@ -26,7 +25,7 @@ class App extends Component {
           <Route path="/chars" component={CharDetails}/>
           <Route path="/emoji" component={EmojiList}/>
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
